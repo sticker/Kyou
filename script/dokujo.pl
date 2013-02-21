ec09379fb34e7c7f423680ddca4e4bdafe332b0e
tetsu-sakura <hoshino.tetsuya@gmail.com>
2013-02-21 19:53:53 +0900

    scraping modified.

--- script/dokujo.pl
+++ script/dokujo.pl
@@ -1,37 +1,75 @@
 #!/usr/bin/perl
 
+#########################################################
+#
+# 毒女ニュース トップ記事取得スクリプト
+#
+#  - 毒女ニューストップページからトップ記事の情報を取得し
+#    JSON形式で出力する
+#  - 形式は以下のとおり
+#
+#    [
+#       {
+#           "url":"記事URL",
+#           "title":"記事タイトル",
+#           "image":"記事表紙画像URL"
+#       },
+#       ・・・
+#    ]
+#
+#########################################################
+
 use strict;
 use warnings;
 
 use YAML;
 use Data::Dumper;
 use Web::Scraper;
+use LWP::UserAgent;
 use URI;
 use JSON;
-use Data::Structure::Util qw(unbless);
 use Encode;
 
+my $url = 'http://www.officiallyjd.com/';
+
+#Scrape設定
 my $scraper = scraper {
     process 'div.description', 'result[]' => scraper {
-        process 'a', 'url' => [ '@href', sub { $_->as_string } ];
-        process 'a>img', 'image' => [ '@src', sub{ $_->as_string } ],
-                       'title' => '@title';
+        process 'a', 'url' => '@href';
+        process 'a>img', 'image' => '@src', 'title' => '@title';
     };
 };
 
-my $uri = new URI('http://www.officiallyjd.com/');
+#UserAgentを変える
+use LWP::UserAgent;
+my $ua = new LWP::UserAgent( agent => 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)');
+$scraper->user_agent($ua);
 
-my $res = $scraper->scrape($uri);
+#外部サイトへリクエスト
+my $response = $scraper->user_agent->get($url);
+#print Dumper $response;
+
+#外部サイトからエラー応答が返ってきたらERROR返却して終了
+unless ($response->is_success) {
+    print "ERROR:remote site is down.";
+    exit 1;
+}
+
+#エンコード指定
+my ($encoding) = $response->header('Content-Type') =~ /charset=([\w\-]+)/g;
+#外部サイトからの応答をScrape
+my $res = $scraper->scrape( Encode::decode($encoding, $response->content));
 
 #print Dumper $res;
 #print "===============================\n\n";
 
 
+#JSON形式で出力
 my $json = new JSON;
-
-my $json_text = '[';
+#my $json_text = '[';
+my $json_text = '';
 $json_text .= $json->encode($res->{result}).', ';
 $json_text =~ s/,.$//s; #最後のカンマを除去
-$json_text .= ']';  #JSONハッシュ配列の閉じ
+#$json_text .= ']';  #JSONハッシュ配列の閉じ
 
 print utf8::is_utf8($json_text) ? encode('utf-8', $json_text) : $json_text;
--- script/girls-mens.pl
+++ script/girls-mens.pl
@@ -1,29 +1,74 @@
 #!/usr/bin/perl
 
+#########################################################
+#
+# 今日の注目ガール・おしゃれ男子 取得スクリプト
+#
+#  - オリコン トレンド トップページから情報を取得し
+#    JSON形式で出力する
+#  - 形式は以下のとおり
+#
+#    [
+#       {
+#           "url":"詳細URL",
+#           "image":"画像URL",
+#       },
+#       ・・・
+#    ]
+#
+#########################################################
+
 use strict;
 use warnings;
 
 use YAML;
 use Data::Dumper;
 use Web::Scraper;
+use LWP::UserAgent;
 use URI;
 use JSON;
+use Encode;
 
+my $url = 'http://www.oricon.co.jp/trend/';
+
+#Scrape設定
 my $scraper = scraper {
-    process 'div.mainPhoto>p>a>img', 'photo[]' => '@src';
-    process 'div.mainPhoto>p>a', 'girls_link[]' => '@href';
+    process 'div.mainPhoto>p>a', 'result[]' => scraper {
+        process 'a', 'url' => '@href';
+        process 'a>img', 'image' => '@src';
+    };
 };
 
-my $uri = new URI('http://www.oricon.co.jp/trend/');
+#UserAgentを変える
+use LWP::UserAgent;
+my $ua = new LWP::UserAgent( agent => 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)');
+$scraper->user_agent($ua);
 
-my $res = $scraper->scrape($uri);
+#外部サイトへリクエスト
+my $response = $scraper->user_agent->get($url);
+#print Dumper $response;
 
-#print Dump $res;
+#外部サイトからエラー応答が返ってきたらERROR返却して終了
+unless ($response->is_success) {
+    print "ERROR:remote site is down.";
+    exit 1;
+}
 
-my $json = '[';
-$json .= (JSON->new->encode($res).', ');
+#エンコード指定
+my ($encoding) = $response->header('Content-Type') =~ /charset=([\w\-]+)/g;
+#外部サイトからの応答をScrape
+my $res = $scraper->scrape( Encode::decode($encoding, $response->content));
 
-$json =~ s/,.$//s; #最後のカンマを除去
-$json .= ']';  #JSONハッシュ配列の閉じ
+#print Dumper $res;
+#print "===============================\n\n";
 
-print $json;
+
+#JSON形式で出力
+my $json = new JSON;
+#my $json_text = '[';
+my $json_text = '';
+$json_text .= $json->encode($res->{result}).', ';
+$json_text =~ s/,.$//s; #最後のカンマを除去
+#$json_text .= ']';  #JSONハッシュ配列の閉じ
+
+print utf8::is_utf8($json_text) ? encode('utf-8', $json_text) : $json_text;
